4c291ad593455d82ecc6c04f8950a1607e9ba90d
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-10-05 17:56:00 -0500

    doing auth.controller.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
 import pkg from '../package.json';
+
+
+
 import productsRoutes from './routes/products.routes';
 import authRoutes from './routes/auth.routes';
+import { createRoles } from './libs/startSetup';
 
 const app = express();
+createRoles();
 
 app.set('pkg', pkg)
 
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/User';
 import jwt from 'jsonwebtoken';
+import config from '../config';
+import Role from "../models/Role";
 
 export const register = async (req, res) => {
    const { username, email, password, roles } = req.body;
@@ -10,9 +12,23 @@ export const register = async (req, res) => {
       password: await User.encryptPassword(password)
    });
 
+   if(roles){
+      const foundRoles = await Role.find({name: {$in: roles}});
+      newUser.roles = foundRoles.map(role => role._id)
+   } else {
+      const role = await Role.findOne({name: "user"});
+      newUser.roles = [role._id];
+   }
+
    const savedUser = await newUser.save();
+   console.log(savedUser);
 
    // Token
+   const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+      expiresIn: 86400 // Parámetro en segundos, equivale a 24 hrs
+   })
+
+   res.status(200).json({token})
 
    await newUser.save()
    res.json("register")
